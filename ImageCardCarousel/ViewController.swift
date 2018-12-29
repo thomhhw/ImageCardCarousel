68f568da2e1f33fcf49b9b5ef958a3d8c9555f72
Thom Pheijffer <tpheijffer@icloud.com>
2018-12-29 16:17:41 +0100

    Initial Commit

---
+++ ImageCardCarousel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageCardCarousel
+//
+//  Created by Thom Pheijffer on 29/12/2018.
+//  Copyright © 2018 Thom Pheijffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
